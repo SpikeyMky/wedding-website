4383978aeeaabdafab8af62bfb5db57fa371cf79
Michael Pallister <michael.pallister@weaveability.com>
2019-09-15 16:39:51 +0100

    Initial react build + component installation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Switch, Route, Redirect} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import Header from './components/header/header.component';
+// import Footer from './components/footer/footer.component';
+
+// import './App.scss';
+import HomePage from './pages/homepage/homepage.component';
+// import ShopPage from './pages/shop/shop.component';
+// import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
+// import {auth, createUserProfileDocument} from './firebase/firebase.utils';
+
+import {connect} from 'react-redux';
+
+// import {setCurrentUser} from './redux/user/user.actions';
+
+
+
+class App extends React.Component {
+
+
+  render() {
+    return (
+      <div>
+        {/* <Header/> */}
+        <Switch>
+          <Route exact path="/" component={HomePage}/>
+          {/* <Route path="/shop" component={ShopPage}/>
+          <Route exact path="/signin" render={()=> this.props.currentUser ? (<Redirect to="/"/>) : (<SignInAndSignUpPage/>)}/> */}
+        </Switch>
+        {/* <Footer/> */}
+      </div>
+    );
+  }
 }
 
-export default App;
+// const mapStateToProps = () => ({
+// });
+// const mapDispatchToProps = dispatch => ({
+// })
+
+export default connect(null, null)(App);
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {} from './homepage.styles';
+
+// import Directory from "../../components/directory/directory.component";
+
+const HomePage = () => (
+    <div className="homepage">
+        <p>Wedding website</p>
+    </div>
+)
+
+export default HomePage;

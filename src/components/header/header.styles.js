ff927c729fde3b53f2c2f79ca2edb78b0798d328
Michael Pallister <burned-is-back@hotmail.co.uk>
2019-09-28 00:57:59 +0200

    logic for hours mins and seconds also

--- src/components/header/header.styles.js
+++ src/components/header/header.styles.js
@@ -2,19 +2,22 @@ import styled from 'styled-components';
 
 
 export const HeaderContainer = styled.header`
-    border-bottom:1px solid #000;
+    border-bottom:1px solid ${props=>props.theme.secondary2};
     position:sticky;
     top:0;
-    background:#fff;
+    background:${props=>props.theme.secondary1};
     text-align:center;
     display:flex;
     align-items:center;
-    box-shadow: 0 2px 17px #000;
+    box-shadow: 0 2px 17px ${props=>props.theme.secondary2};
 `;
 
 export const HeaderContent = styled.div`
     flex:1;
     padding:5px 0;
+    justify-content:space-around;
+    align-items:center;
+    display:flex;
 `;
 
 export const CladdaghImage = styled.img`
@@ -23,7 +26,7 @@ export const CladdaghImage = styled.img`
 `;
 export const ElvinSubText = styled.p`
     font-family:'ElvinBI';
-    color:#008000;
+    color:${props=>props.theme.primary2};
     margin:0;
 `;
 export const BouquetsLeft = styled.img`
